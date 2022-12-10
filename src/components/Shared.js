6292e977662f50d6353881e057a99a8aa1ae3ab5
unknown <iury.flores@gmail.com>
2022-12-10 20:37:51 -0300

    Feat:Defendant page finished

--- src/components/Shared.js
+++ src/components/Shared.js
@@ -17,69 +17,72 @@ export const LinkNav = styled(Link)`
   }
 `;
 export const Button = styled(Link)`
-  
   text-decoration: none;
-  position:absolute;
-  top:80px;
-  font-size:2rem;
-  right:20px;
+  position: absolute;
+  top: 80px;
+  font-size: 2rem;
+  right: 20px;
   color: inherit;
-  width:50px;
-  height:50px;
-  background-color:#4682b4;
+  width: 50px;
+  height: 50px;
+  background-color: #4682b4;
   border-radius: 30px;
   border: 0;
   &:hover {
     cursor: pointer;
     background-color: white;
-    color:#4682b4;
+    color: #4682b4;
     transition: all 0.5s;
   }
 `;
 
 export const MsgError = styled.div`
-background-color:#FA8072;
-padding:5px 20px;
-margin-bottom:10px;
-border-radius:5px;
-`
+  background-color: #fa8072;
+  padding: 5px 20px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
 export const MsgSucess = styled.div`
-background-color:#AFE1AF;
-padding:5px 20px;
-margin-bottom:10px;
-border-radius:5px;
-`
+  background-color: #afe1af;
+  padding: 5px 20px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
 export const DefendantCard = styled(Link)`
-border:1px solid #4682b4;
-padding:10px 15px;
-background-color:aliceblue;
-color:#0F52BA;
-display:flex;
-width:90vw;
-justify-content:space-between;
-align-items:center;
-text-decoration:none;
-margin-top:5px;
-border-radius:10px;
-&:hover {
-  background-color:#4682b4;
-  color:white;
-}
-`
+  border: 1px solid #4682b4;
+  padding: 10px 15px;
+  background-color: aliceblue;
+  color: #0f52ba;
+  display: flex;
+  width: 90vw;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  margin-top: 5px;
+  border-radius: 10px;
+  &:hover {
+    background-color: #4682b4;
+    color: white;
+  }
+  p {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 export const ButtonView = styled(Link)`
-background-color:aliceblue;
-padding: 10px 20px;
-display:flex;
-align-items:center;
-justify-content:center;
-text-decoration:none;
-margin-top:5px;
-border-radius:10px;
-border:2px solid transparent;
-color:#0f52ba;
-&:hover {
-  background-color:#4682b4;
-  color:white;
-  border:2px solid white;
-}
-`
+  background-color: aliceblue;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  margin-top: 5px;
+  border-radius: 10px;
+  border: 2px solid transparent;
+  color: #0f52ba;
+  &:hover {
+    background-color: #4682b4;
+    color: white;
+    border: 2px solid white;
+  }
+`;
--- src/pages/DefendantProcess.js
+++ src/pages/DefendantProcess.js
@@ -1,14 +1,23 @@
 import React from "react";
 import api from "../utils/api.utils";
 import { useState, useEffect } from "react";
-import { MsgSucess, MsgError, DefendantCard } from "../components/Shared";
+import { DefendantCard } from "../components/Shared";
 import { useParams } from "react-router-dom";
 
 export const DefendantProcess = ({ message, setMessage }) => {
   const { id } = useParams();
 
   const [lawSuit, setLawSuit] = useState([]);
-  const [error, setError] = useState(null);
+  const [defendant, setDefendant] = useState("");
+
+  const getDefendant = async () => {
+    try {
+      const data = await api.getOneDefendant(id);
+      setDefendant(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getAllProcessDefendant = async () => {
     try {
@@ -19,22 +28,39 @@ export const DefendantProcess = ({ message, setMessage }) => {
     }
   };
   useEffect(() => {
-    getAllProcessDefendant();
+    getDefendant();
   }, []);
+  useEffect(() => {
+    getAllProcessDefendant();
+  });
   return (
     <div>
+      <h3 style={{ display: "flex", flexDirection: "column" }}>
+        <span>All process from</span>
+        <span>{defendant.full_name}</span>
+        <span>({defendant.cnpj})</span>
+      </h3>
       {lawSuit.map((process, index) => {
         return (
-          <DefendantCard key={index}>
+          <DefendantCard key={index} to={`/process/${process._id}`}>
+            <i className="bi bi-folder"></i>
+            <p>
+              Date:{" "}
+              <b>
+                {
+                  (process.dateProcess = new Date(
+                    process.dateProcess
+                  ).toLocaleDateString())
+                }
+              </b>
+            </p>
             <p>
               Process number: <b> {process.processNumber}</b>
             </p>
             <p>
               Complainant: <b> {process.complainantName}</b>
             </p>
-            <p>
-              Defendant: <b> {process.defendantId.full_name}</b>
-            </p>
+            <i className="bi bi-box-arrow-right"></i>
           </DefendantCard>
         );
       })}
