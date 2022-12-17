a4697e71d4e6e16133cf66b26cd36fa39b260992
unknown <iury.flores@gmail.com>
2022-12-17 08:40:05 -0300

    Feat:msg in process when there is no process

--- src/pages/Process.js
+++ src/pages/Process.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import api from "../utils/api.utils";
-import { MsgSucess, DefendantCard } from "../components/Shared";
+import { MsgSucess, DefendantCard, ButtonView } from "../components/Shared";
+
 
 export const Process = ({ message, setMessage, loading, setLoading }) => {
   const [lawsuit, setLawSuit] = useState([]);
@@ -29,7 +30,7 @@ export const Process = ({ message, setMessage, loading, setLoading }) => {
       {message !== null && <MsgSucess>{message}</MsgSucess>}
 
       <h3>All Process</h3>
-      {lawsuit.length <= 0 && <h3> No process registred!</h3>}
+      {lawsuit.length <= 0 && <><h3> No process registred!</h3><h4>To add a process you must add a defendant first.</h4><ButtonView to='/add-defendant/'>Add Defendant</ButtonView></>}
       {lawsuit.map((process, index) => (
         <DefendantCard to={`/process/${process._id}`} key={index}>
           <p>
